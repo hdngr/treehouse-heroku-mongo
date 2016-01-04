11fe2dc0211df4b72f21911375a6fed97a8bb206
hdngr <hstnhdngr@gmail.com>
2016-01-04 11:03:14 -0800

    use localhost

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const http = require('http');
 
-const hostname = '127.0.0.1';
 const port = process.env.PORT || 1337;
 
 http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello World\n');
-}).listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+}).listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
 });
