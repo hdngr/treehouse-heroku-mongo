8df21854bb2a31dc7ba9275288bb75a40fb2e88d
hdngr <hstnhdngr@gmail.com>
2016-01-04 10:57:37 -0800

    use heroku's ports

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const hostname = '127.0.0.1';
-const port = 1337;
+const port = process.env.PORT || 1337;
 
 http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
